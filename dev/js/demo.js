26453fac2c080c87ea5c8b0a6227b0573d2c514e
kathyvychung <kvce33@drexel.edu>
2020-09-29 13:34:53 -0400

    commit

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -5,7 +5,19 @@ import {gsap} from "gsap";
     Demo Script
 ----------------- */
 
-var timeOfAnimation = 3;
+var timeOfAnimation = 2;
 
-gsap.to(".red-box",{duration: timeOfAnimation,borderRadius: "20"});
-gsap.to(".red-box",{duration: timeOfAnimation, rotation:360, repeat:3, delay:timeOfAnimation });
+gsap.to(".stick",{duration: timeOfAnimation, rotation: 360, x:500, y:400});
+
+gsap.from(".circle-1",{duration: timeOfAnimation, x:-800, y:300, delay:2});
+gsap.to(".circle-1",{duration: timeOfAnimation, x:800, y:300, delay:2});
+
+gsap.from(".circle-2",{duration: timeOfAnimation, x:-700, y:100, delay:3 });
+gsap.to(".circle-2",{duration: timeOfAnimation, x:700, y:100, delay:3 });
+
+gsap.from(".circle-3",{duration: timeOfAnimation, x:-550, y:-100, delay:4 });
+gsap.to(".circle-3",{duration: timeOfAnimation, x:550, y:-100, delay:4 });
+
+gsap.to(".circle-3",{duration: 1, alpha:0, delay:6});
+gsap.to(".circle-2",{duration: 1, alpha:0, delay:7});
+gsap.to(".circle-1",{duration: 1, alpha:0, delay:8});
