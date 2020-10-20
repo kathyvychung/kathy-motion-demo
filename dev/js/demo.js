375897907eb16106b7f5c51dcd586c47b6ccd4c7
kathyvychung <kvce33@drexel.edu>
2020-10-20 19:50:17 -0400

    close to done

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -9,9 +9,12 @@ giraffeTL.from("#neck",{duration: 5, y: 600, ease: "none"}, "swing")
         .from("#numberFill",{duration: 4, y: 500, ease: "none"}, "swing")
         .from("#tail",{rotation:20, transformOrigin:"left top"}, "swing")
         .from("#tail",{rotation:0, transformOrigin:"left top"}, "swing")
-        .to("#svg-demo",{ duration: 0.2, alpha: 0, ease: "none"})
-        .to("#bush",{ duration: 0.2, alpha: 1, ease: "none"})
-        // .to("#neck",{ duration: 1, alpha: 1, y: 400});
+        .to("#part1",{ duration: 0.1, alpha: 0})
+        .from("#goal",{alpha:0})
+        .from("#neck2",{duration: 3, y: 400},"rise")
+        .from("#numberFill2",{duration: 2.5, y: 500,},"rise");
+        .to("#ear2",{rotation:20, transformOrigin:"left top"})
+
 
 export function giraffeAnimation(){
     return giraffeTL;
