e0a104c41c155ea6b0870034e0692829de101f60
kathyvychung <kvce33@drexel.edu>
2020-10-20 15:39:14 -0400

    bush svg

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -5,8 +5,13 @@ gsap.set("#neck",{transformOrigin: "center"})
     Demo Script
 ----------------- */
 const giraffeTL = gsap.timeline();
-// giraffeTL.from("#neck",{duration: 2, y: 600, ease: "none"})
-        // .to("#neck",{y: -600, ease: "none"});
+giraffeTL.from("#neck",{duration: 5, y: 600, ease: "none"}, "swing")
+        .from("#numberFill",{duration: 4, y: 500, ease: "none"}, "swing")
+        .from("#tail",{rotation:20, transformOrigin:"left top"}, "swing")
+        .from("#tail",{rotation:0, transformOrigin:"left top"}, "swing")
+        .to("#svg-demo",{ duration: 0.2, alpha: 0, ease: "none"})
+        .to("#bush",{ duration: 0.2, alpha: 1, ease: "none"})
+        // .to("#neck",{ duration: 1, alpha: 1, y: 400});
 
 export function giraffeAnimation(){
     return giraffeTL;
